2723be51812b5ebd6a71598cfdcd65b8798341fb
MakariysBigys <bombila96@mail.ru>
2022-06-03 13:51:13 +0300

    Add test for password

--- UnitLessonTests/ValidationServiceTests.swift
+++ UnitLessonTests/ValidationServiceTests.swift
@@ -67,4 +67,54 @@ class ValidationServiceTests: XCTestCase {
 
     }
     
+    func testValidatePasswordIsValidatePassword() throws {
+            let result = try validationService.validatePassword("asdf343ffff")
+            XCTAssertNoThrow(result)
+        }
+        
+    func testValidatePasswordIsInvalideValue() throws {
+        let expectedError = ValidationError.invalidValue
+        var error: ValidationError?
+        
+        XCTAssertThrowsError(try validationService.validatePassword(nil))
+        {
+            thrownError in
+            error = thrownError as? ValidationError
+        }
+        
+        XCTAssertEqual(expectedError, error)
+        XCTAssertEqual(expectedError.errorDescription, error?.errorDescription)
+        
+    }
+    
+    func testValidatePasswordTooShort() throws {
+        let expectedError = ValidationError.passwordToShort
+        var error: ValidationError?
+        
+        XCTAssertThrowsError(try validationService.validatePassword("1234567"))
+        {
+            thrownError in
+            error = thrownError as? ValidationError
+        }
+        
+        XCTAssertEqual(expectedError, error)
+        XCTAssertEqual(expectedError.errorDescription, error?.errorDescription)
+ 
+    }
+    
+    func testValidationPasswordTooLong() throws {
+        let expectedError = ValidationError.passwordToLong
+        var error: ValidationError?
+        
+        XCTAssertThrowsError(try validationService.validatePassword("245345234245fdbxbvxfgfb43534bvcxbcvxbxvbxcvb4545vbx"))
+        {
+            thrownError in
+            error = thrownError as? ValidationError
+        }
+        
+        XCTAssertEqual(expectedError, error)
+        XCTAssertEqual(expectedError.errorDescription, error?.errorDescription )
+        
+    }
+    
 }
